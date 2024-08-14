ddbb62f2462491d12e181b9ade25ded83215cd82
Arjun Singh <aks99034@gmail.com>
2024-08-14 19:54:28 +0545

    frist commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('commentForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your comment!');
+    // Here, you can add code to send the form data to a server or handle it as needed.
+});
